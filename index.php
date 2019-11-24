a0e916a21756175265809d9387cb8398369a13d5
Sir Louis <me@sirlouis.io>
2019-11-24 01:34:53 +0100

    bugs fixed

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ require_once APP_BASE_INC;
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
+    <?php \setup_page('terms','base', true); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
       <div class="view-space">
